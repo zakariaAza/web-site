9b61401d5c3a981796020e2b9c16ac34348d7c14
zakariaAza <zakaria.azakkour@outlook.fr>
2019-10-07 09:45:00 +0200

    Add an introduction to the home page

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <Carousel/>
+        <p class="text-center"> Etabli en France depuis 2019, notre cabinet conseille les directions générales de grandes entreprises françaises et internationales, ainsi que celles d’institutions publiques et d’organisations à but non lucratif. Le cabinet inscrit l’accompagnement de ses clients dans le long terme. Il les aide à élaborer les orientations stratégiques et à mettre en œuvre les changements qui leur permettent d’améliorer durablement leurs performances. Notre cabinet, offre à ses clients les compétences de 3 grands consultants experts ..</p>
         <AppFooter/>
     </div>
 </template>
@@ -14,3 +15,15 @@ export default {
 
 }
 </script>
+
+<style scoped>
+  .text-center {
+    position: relative;
+    color: black;
+    max-width: 500px;
+    top: 320px;
+    font-size: 20px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
