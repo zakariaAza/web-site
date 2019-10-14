495de30bab909a7329f57acee90528c8ff6755ca
Desmond <Conteh@et.esiea.fr>
2019-10-15 01:13:22 +0200

    Components: add Contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,27 +1,89 @@
 <template>
-    <v-container>
+    <v-container >
       <h3 class="pt-5 blue--text text--darken-4">A propos de DCA Consulting corporation</h3>
-      <h1 class="pt-5 Contact">Contactez-Nous</h1>
-      <p class="pt-3 TextContact">
+      <h1 class="pt-5 ContactTitle">Contactez-Nous</h1>
+      <p class="pt-3 pb-10 TextContact">
         Nous vous remercions de l’intérêt que vous portez à <strong class="blue--text text--darken-4">DCA Consulting corporation</strong>.<br>
         Vous pouvez nous contacter à l’aide des coordonnées indiquées ci-dessous.
-      </p>
+      </p >
 
-      <v-layout class="pt-10 pb-10" row wrap>
-        <v-flex xs12 sm6 md4 v-for="information in contact" :key="information.id">
-          <v-card  width="400" height="250px" color="" flat class="text-xs-center">
-            <v-card-title primary class="size" >  <strong class="blue--text text--darken-4">{{ information.Title }}</strong></v-card-title>
-            <v-card-text>
-              <!-- bureau de paris -->
-              <br><div class="blue--text text--darken-4" >{{information.name}}</div>
-              <div>{{information.adresse}}</div>
-              <div>{{information.codePostal}}</div>
-              <div>{{information.pays}}</div><br>
-              <div>{{information.Telephone}}</div>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
+        <v-layout row wrap class="pb-8">
+           <v-flex xs6 md4 v-for="information in contact" :key="information.id">
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong> Bureau de Paris</strong></v-card-title>
+              <div class="ml-3 pt-3">{{information.name}}</div>
+              <div class="ml-3">{{information.adresse}}</div>
+              <div class="ml-3">{{information.codePostal}}</div>
+              <div class="ml-3">{{information.pays}}</div><br>
+              <div class="ml-3">{{information.Telephone}}</div>
+             </v-card>
+          </v-flex>
+          <v-flex xs6 md4>
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong>Demande de mission de conseil</strong> </v-card-title>
+              <v-card-text>
+                <v-btn text class="padding size2" color="blue darken-4"><v-icon>mdi-menu-right </v-icon>Soumettre votre requête</v-btn>
+              </v-card-text>
+             </v-card>
+          </v-flex>
+          <v-flex xs6 md4>
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong> Réseaux sociaux</strong></v-card-title>
+              <v-card-text>
+                <v-btn text class="padding size2" color="blue darken-4"><v-icon>mdi-menu-right </v-icon>Suivez-nous sur Facebook, Twitter et Instagram</v-btn>
+              </v-card-text>
+             </v-card>
+          </v-flex>
+          <v-flex xs6 md4>
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong>Relations Presse</strong>  </v-card-title>
+              <v-card-text class="mt-5 "><a href="mailto:conteh@et.esiea.fr">conteh@et.esiea.fr</a></v-card-text>
+              <v-card-text class="mt-n10"><a href="mailto:soumaila@et.esiea.fr">soumaila@et.esiea.fr</a></v-card-text>
+              <v-card-text class="mt-n10"><a href="mailto:azakkour@et.esiea.fr">azakkour@et.esiea.fr</a></v-card-text>
+              <v-card-text class="mt-n5"><a href="mailto:azakkour@et.esiea.fr">Paris.Marketing@DCACorporation.fr</a></v-card-text>
+             </v-card>
+          </v-flex>
+          <v-flex xs6 md4>
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong>Recrutement</strong> </v-card-title>
+              <v-card-text class="mt-5 "><a href="mailto:soumaila@et.esiea.fr">soumaila@et.esiea.fr</a></v-card-text>
+              <v-card-text class="mt-n10"><a href="mailto:azakkour@et.esiea.fr">azakkour@et.esiea.fr</a></v-card-text>
+              <v-card-text class="mt-n5"><a href="mailto:azakkour@et.esiea.fr">Paris.Recruiting@DCACorporation.fr</a></v-card-text>
+             </v-card>
+          </v-flex>
+          <v-flex xs6 md4>
+             <v-card outlined  width="400" height="250px">
+              <v-card-title class="size blue--text text--darken-4"> <strong>Question ou commentaires</strong>  </v-card-title>
+              <v-card-text>
+                <v-btn text class="padding size2" color="blue darken-4"><v-icon>mdi-menu-right </v-icon><a href="mailto:Paris.Recruiting@DCACorporation.fr">Contacter-Nous</a></v-btn>
+              </v-card-text>
+             </v-card>
+          </v-flex>
+        </v-layout>
+
+      <p class="pt-3 pb-10 TextContact">
+        "Les meilleures choses qui arrivent dans le monde de l’entreprise ne sont pas le résultat du travail d’un seul homme. C’est le travail de toute une équipe." <br><br><strong>Steve JOBS</strong>
+      </p >
+
+         <v-row>
+      <v-col cols="6" sm="4">
+        <v-img
+          src="https://cdn.pixabay.com/photo/2017/04/03/17/24/teamwork-2198961_1280.png"
+        ></v-img>
+      </v-col>
+
+      <v-col cols="6" sm="4">
+        <v-img src="https://cdn.pixabay.com/photo/2018/03/28/09/44/document-3268750_1280.jpg">
+          <div class="fill-height bottom-gradient"></div>
+        </v-img>
+      </v-col>
+
+      <v-col cols="6" sm="4">
+        <v-img src="https://cdn.pixabay.com/photo/2016/12/19/10/16/hand-1917895_1280.png">
+          <div class="fill-height repeating-gradient"></div>
+        </v-img>
+      </v-col>
+    </v-row>
     </v-container>
 </template>
 
@@ -30,12 +92,7 @@ export default {
   data () {
     return {
       contact: [
-        { Title: 'Bureau de Paris', name: 'DCA consulting Corporation', adresse: '1 Infinite Loop', codePostal: 'CA 95014', pays: 'Etats-Unis', Telephone: 'Téléphone : +33 1 44 55 75 75' },
-        { Title: 'Demande de conseil', text: 'Soumettre votre requête', icon: 'mdi-account' },
-        { Title: 'Réseaux sociaux', media: 'Suivez-nous sur LinkedIn, Facebook, Twitter et YouTube.' },
-        { Title: 'Relations Presse ' },
-        { Title: 'Recrutement ' },
-        { Title: 'Question ou commentaires ', text: 'contactez-nous' }
+        { name: 'DCA consulting Corporation', adresse: '1 Infinite Loop', codePostal: 'CA 95014', pays: 'Etats-Unis', Telephone: 'Téléphone : +33 1 44 55 75 75' }
       ]
     }
   }
@@ -43,13 +100,19 @@ export default {
 </script>
 
 <style scoped>
-.Contact {
+.ContactTitle {
   font-size: 40px
 }
 .TextContact{
   font-size: 30px
 }
-.size{
-  font-size: 30px
+.padding {
+  bottom: -100px;
+}
+.size {
+   font-size: 25px
+}
+.size2 {
+   font-size: 12px
 }
 </style>
