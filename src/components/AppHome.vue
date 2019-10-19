7c210cbe604fe8748f622eecc947558481de33a5
zakariaAza <zakaria.azakkour@outlook.fr>
2019-10-19 19:29:45 +0200

    Remove the footer from the home page

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -4,18 +4,16 @@
         <p class="text-center"> Etabli en France depuis 2019, notre cabinet conseille les directions générales de grandes entreprises françaises et internationales, ainsi que celles d’institutions publiques et d’organisations à but non lucratif. Le cabinet inscrit l’accompagnement de ses clients dans le long terme. Il les aide à élaborer les orientations stratégiques et à mettre en œuvre les changements qui leur permettent d’améliorer durablement leurs performances. Notre cabinet, offre à ses clients les compétences de 3 grands consultants experts ..</p>
         <VerticalSeparator/>
         <TeamCard class="team"/>
-        <AppFooter/>
     </div>
 </template>
 
 <script>
 import VerticalSeparator from './VerticalSeparator'
 import Carousel from './Carousel'
-import AppFooter from './AppFooter'
 import TeamCard from './TeamCard'
 
 export default {
-  components: { AppFooter, Carousel, VerticalSeparator, TeamCard }
+  components: { Carousel, VerticalSeparator, TeamCard }
 
 }
 </script>
